3baf028845a2a2fdc7193ad0e57faac09e153403
wytzepiet <wytzepiet@gmail.com>
2026-02-24 16:12:52 +0100

    nice default map pin

--- src/index.tsx
+++ src/index.tsx
@@ -41,6 +41,7 @@ export interface GoogleMapProps {
   zoom?: number;
   children?: React.ReactNode;
   showControls?: boolean;
+  className?: string;
 }
 
 export const GoogleMap: React.FC<GoogleMapProps> = ({
@@ -51,11 +52,12 @@ export const GoogleMap: React.FC<GoogleMapProps> = ({
   zoom = 12,
   children,
   showControls = true,
+  className,
 }) => {
   return (
     <APIProvider apiKey={apiKey}>
       <Map
-        style={{ width: "100%", height: "100%" }}
+        className={className}
         defaultCenter={{ lat: centerLat, lng: centerLng }}
         defaultZoom={zoom}
         mapId={mapId}
@@ -120,7 +122,7 @@ export function registerGoogleMap(loader: PlasmicComponentLoader) {
           },
           {
             type: "img",
-            src: "/pin.png",
+            src: "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 640 640'%3E%3Cpath fill='rgb(230,99,99)' d='M128 252.6C128 148.4 214 64 320 64C426 64 512 148.4 512 252.6C512 371.9 391.8 514.9 341.6 569.4C329.8 582.2 310.1 582.2 298.3 569.4C248.1 514.9 127.9 371.9 127.9 252.6zM320 320C355.3 320 384 291.3 384 256C384 220.7 355.3 192 320 192C284.7 192 256 220.7 256 256C256 291.3 284.7 320 320 320z'/%3E%3C/svg%3E",
             styles: {
               width: "45px",
               display: "block",
